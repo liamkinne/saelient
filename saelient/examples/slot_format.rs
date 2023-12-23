9d69545dae7c5bcc8481397c0cb623b634c9015b
Liam Kinne <liam@kinne.io>
2023-12-24 01:32:31 +1000

    make normal comment

--- saelient/examples/slot_format.rs
+++ saelient/examples/slot_format.rs
@@ -4,6 +4,6 @@ use saelient::slot::SAEaa01;
 fn main() {
     let slot = SAEaa01::new(20.0).unwrap();
 
-    /// When the `std` feature is enabled, you can `Display` the value of a slot with it's units.
+    // When the `std` feature is enabled, you can `Display` the value of a slot with it's units.
     println!("Output: {}", slot);
 }
