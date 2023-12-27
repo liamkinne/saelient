b9800a36db6dd0ec8be3794462015b916d9177eb
Liam Kinne <liam@kinne.io>
2023-12-28 04:48:43 +1000

    make methods public

--- saelient/src/name.rs
+++ saelient/src/name.rs
@@ -4,7 +4,7 @@ struct Name([u8; 8]);
 
 impl Name {
     /// Identity number.
-    fn identity(&self) -> u32 {
+    pub fn identity(&self) -> u32 {
         // first 21 bits
         ((self.0[2] as u32) & 0b0001_1111) << 16
             | (self.0[1] as u32) << 8
@@ -12,45 +12,45 @@ impl Name {
     }
 
     /// Manufacturer code.
-    fn manufacturer(&self) -> u16 {
+    pub fn manufacturer(&self) -> u16 {
         // bits 22 to 31
         ((self.0[2] as u16) >> 5) | ((self.0[3] as u16) << 3)
     }
 
     /// ECU instance.
-    fn ecu_instance(&self) -> u8 {
+    pub fn ecu_instance(&self) -> u8 {
         // bits 32 to 34
         self.0[4] & 0b0000_0111
     }
 
     /// Function instance.
-    fn function_instance(&self) -> u8 {
+    pub fn function_instance(&self) -> u8 {
         // bits 34 to 40
         (self.0[4]) >> 3 & 0b0011_1111
     }
 
     /// Function.
-    fn function(&self) -> Option<u8> {
+    pub fn function(&self) -> Option<u8> {
         Some(self.0[5])
     }
 
     /// Vehicle system.
-    fn vehicle_system(&self) -> Option<u8> {
+    pub fn vehicle_system(&self) -> Option<u8> {
         Some((self.0[6] >> 1) & 0b0111_1111)
     }
 
     /// Vehicle system instance.
-    fn vehicle_system_instance(&self) -> u8 {
+    pub fn vehicle_system_instance(&self) -> u8 {
         self.0[7] & 0b0000_1111
     }
 
     /// Industry group.
-    fn industry_group(&self) -> Option<IndustryGroup> {
+    pub fn industry_group(&self) -> Option<IndustryGroup> {
         IndustryGroup::try_from((self.0[7] >> 4) & 0b0000_0111).ok()
     }
 
     /// Arbitrary address capable.
-    fn arbitrary_address_capable(&self) -> bool {
+    pub fn arbitrary_address_capable(&self) -> bool {
         (self.0[7] >> 7) == 1
     }
 }
